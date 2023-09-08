41dfa9977169b81f50fc9f2fdc7c299b9f0faa5b
Alex Muns <a.muns@live.ca>
2023-09-08 16:40:28 -0400

    Added loadDBData and ListView adapter to VarietyActivity

--- app/src/main/java/com/example/fruitgrowth/VarietyActivity.java
+++ app/src/main/java/com/example/fruitgrowth/VarietyActivity.java
@@ -4,10 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class VarietyActivity extends AppCompatActivity {
+
+    /**
+     * Globals
+     */
+    private ArrayList<String> varietyArray = new ArrayList<>();
+    private ArrayAdapter<String> varietyAdapter;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +34,20 @@ public class VarietyActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.variety_toolbar);
 
         // TODO: Populate ListView from DB
+        loadVarietyData();
+        ListView varietyListView = findViewById(R.id.variety_ListView);
+        varietyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, varietyArray);
+        varietyListView.setAdapter(varietyAdapter);
 
-        // TODO: Set listview item onclick to startActivity(fruitletActivity.class) plus extras
+        // TODO: Test onClick any ListView Item
+        varietyListView.setOnItemClickListener((list, item, position, id) -> {
+            String varietyName = varietyArray.get(position);
+
+            // Place extra into an intent to FruitletActivity
+            Intent newActivity = new Intent(this, FruitletActivity.class);
+            newActivity.putExtra("varietyName", varietyName);
+            startActivity(newActivity);
+        });
 
         // Set buttons' on-clicks
         Button addVarietyBtn = findViewById(R.id.variety_btnAddVariety);
@@ -39,7 +65,24 @@ public class VarietyActivity extends AppCompatActivity {
             startActivity(manageIntent);
         });
 
+    }
 
+    /**
+     * Load variety names into ListView
+     */
+    public void loadVarietyData() {
+        DBOpener dbOpener = new DBOpener(this);
+        db = dbOpener.getWritableDatabase();
+        Cursor results = db.rawQuery("SELECT " + DBOpener.VARIETY_NAME + " FROM " + DBOpener.TABLE_VARIETY + ";", null);
+
+        while (results.moveToNext()) {
+            try {
+                String name = results.getString(0);
+                varietyArray.add(name);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
