5224f881549aaf47bc43213a3ab20ad1eb9095a8
Alex Muns <a.muns@live.ca>
2023-12-07 20:04:50 -0500

    Refactored DB to include Date and TreeDateInt tables

--- app/src/main/java/com/example/fruitgrowth/DBOpener.java
+++ app/src/main/java/com/example/fruitgrowth/DBOpener.java
@@ -10,7 +10,7 @@ public class DBOpener extends SQLiteOpenHelper {
      * DB Constants
      */
     protected final static String DATABASE_NAME = "FruitGrowth_DB";
-    protected final static int VERSION_NUM = 3;
+    protected final static int VERSION_NUM = 4;
 
     // Variety Table
     public final static String TABLE_VARIETY = "Variety";
@@ -23,14 +23,21 @@ public class DBOpener extends SQLiteOpenHelper {
     // Tree Table
     public final static String TABLE_TREE = "Tree";
     public final static String TREE_ID = "TreeID";
-    public final static String TREE_DATE = "Date";
+    public final static String TREE_TREENUMBER = "TreeNumber";
     public final static String TREE_TREEID_FK = "Tree_TreeID";
-    public final static String TREE_DATE_FK = "Tree_Date";
+
+    // Tree_Date_Int Table
+    public final static String TABLE_TREEDATEINT = "TreeDateInt";
+    public final static String TREEDATEINT_ID = "TreeDateIntID";
+    public final static String TREEDATEINT_ID_FK = "TreeDateInt_TreeDateIntID";
+
+    // Date Table
+    public final static String TABLE_DATE = "Date";
+    public final static String DATE_DATE = "Date_Date";
 
     // Fruitlet Table
     public final static String TABLE_FRUITLET = "Fruitlet";
     public final static String FRUITLET_ID = "FruitletID";
-    public final static String FRUITLET_TREENUMBER = "TreeNumber";
     public final static String FRUITLET_CLUSTERNUMBER = "ClusterNumber";
     public final static String FRUITLET_FRUITLETNUMBER = "FruitletNumber";
     public final static String FRUITLET_SIZE = "Size";
@@ -38,7 +45,6 @@ public class DBOpener extends SQLiteOpenHelper {
     // Cluster Table
     public final static String TABLE_CLUSTER = "Cluster";
     public final static String CLUSTER_ID = "ClusterID";
-    public final static String CLUSTER_TREENUMBER = "TreeNumber";
     public final static String CLUSTER_REGION = "Region";
     public final static String CLUSTER_COUNT = "Count";
 
@@ -65,35 +71,41 @@ public class DBOpener extends SQLiteOpenHelper {
         // TODO: Date should be refactored, as one tree can have multiple dates
         db.execSQL("CREATE TABLE " + TABLE_TREE + " ( " +
                 TREE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                TREE_DATE + " DATE, " +
                 VARIETY_VARIETYID_FK + " INTEGER, " +
                 " FOREIGN KEY ( " + VARIETY_VARIETYID_FK + " ) " +
                 " REFERENCES " + TABLE_VARIETY + " ( " + VARIETY_ID + " ));");
 
+        db.execSQL("CREATE TABLE " + TABLE_DATE + " ( " +
+                DATE_DATE + " DATE PRIMARY KEY AUTOINCREMENT, " +
+                VARIETY_VARIETYID_FK + " INTEGER);");
+
+        db.execSQL("CREATE TABLE " + TABLE_TREEDATEINT + " ( " +
+                TREEDATEINT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TREE_TREEID_FK + " INTEGER, " +
+                // VarietyID FK here attached to the TreeID
+                DATE_DATE + " DATE, " +
+                TREE_TREEID_FK + " INTEGER, " +
+                " FOREIGN KEY ( " + TREE_TREEID_FK + " ) " +
+                " REFERENCES " + TABLE_TREE + " ( " + TREE_TREEID_FK + " ), " +
+                " FOREIGN KEY ( " + DATE_DATE + " ) " +
+                " REFERENCES " + TABLE_DATE + " ( " + DATE_DATE + " ));");
+
         db.execSQL("CREATE TABLE " + TABLE_FRUITLET + " ( " +
                 FRUITLET_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                FRUITLET_TREENUMBER + " INTEGER, " +
                 FRUITLET_CLUSTERNUMBER + " INTEGER, " +
                 FRUITLET_FRUITLETNUMBER + " INTEGER, " +
                 FRUITLET_SIZE + " REAL, " +
-                TREE_TREEID_FK + " INTEGER, " +
-                TREE_DATE_FK + " DATE, " +
-                " FOREIGN KEY ( " + TREE_TREEID_FK + " ) " +
-                " REFERENCES " + TABLE_TREE + " ( " + TREE_TREEID_FK + " ), " +
-                " FOREIGN KEY ( " + TREE_DATE_FK + " ) " +
-                " REFERENCES " + TABLE_TREE + " ( " + TREE_DATE_FK + " ));");
+                TREEDATEINT_ID + " INTEGER, " +
+                " FOREIGN KEY ( " + TREEDATEINT_ID + " ) " +
+                " REFERENCES " + TABLE_TREEDATEINT + " ( " + TREEDATEINT_ID_FK + " ));");
 
         db.execSQL("CREATE TABLE " + TABLE_CLUSTER + " ( " +
                 CLUSTER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                CLUSTER_TREENUMBER + " INTEGER, " +
                 CLUSTER_REGION + " TEXT, " +
                 CLUSTER_COUNT + " INTEGER, " +
-                TREE_TREEID_FK + " INTEGER, " +
-                TREE_DATE_FK + " DATE, " +
-                " FOREIGN KEY ( " + TREE_TREEID_FK + " ) " +
-                " REFERENCES " + TABLE_TREE + " ( " + TREE_TREEID_FK + " ), " +
-                " FOREIGN KEY ( " + TREE_DATE_FK + " ) " +
-                " REFERENCES " + TABLE_TREE + " ( " + TREE_DATE_FK + " ));");
+                TREEDATEINT_ID + " INTEGER, " +
+                " FOREIGN KEY ( " + TREEDATEINT_ID + " ) " +
+                " REFERENCES " + TABLE_TREEDATEINT + " ( " + TREEDATEINT_ID_FK + " ));");
     }
 
     /**
@@ -110,6 +122,8 @@ public class DBOpener extends SQLiteOpenHelper {
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_TREE);
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_FRUITLET);
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_CLUSTER);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_DATE);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_TREEDATEINT);
             // Create new db
             onCreate(db);
         }
